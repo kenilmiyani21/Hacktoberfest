72edb9a6b60770972dd7781f72832f8d6ee6cdbe
Aryan khunt <86661610+aryankhunt30@users.noreply.github.com>
2022-10-01 02:21:01 +0530

    Update filter map reduce.py

--- filter map reduce.py
+++ filter map reduce.py
@@ -18,7 +18,7 @@ Iterable = It is maybe either a squence, list, string, tuple, a container which 
 a = [10,40,20,70,30,90,100,60]
 
 def highest_marks(n):
-	if n >= 60:
+	if n >= 78:
 		return True
 
 result = filter(highest_marks,a)
@@ -33,7 +33,7 @@ print(type(result))
 
 # or
 
-result = list(filter(lambda n : (n >= 60),a))
+result = list(filter(lambda n : (n >= 78),a))
 print(result)
 print(type(result))
 
